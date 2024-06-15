851b407e4f98942507dbc466f2fa017d4adf0082
StrippoliJules <julesstrippoli@gmail.com>
2024-06-15 13:32:47 +0200

    Feat: Use vuetify for style

--- src/views/AuthPages/LoginView.vue
+++ src/views/AuthPages/LoginView.vue
@@ -1,32 +1,51 @@
 <template>
-  <div id="app">
-    <div class="login-container">
-      <div class="login-box">
-        <h2>Login</h2>
-        <form @submit.prevent="login">
-          <div class="input-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" v-model="form.email" required />
-          </div>
-          <div class="input-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              v-model="form.password"
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        <p class="switch-link">
-          Don't have an account?
-          <router-link to="/register" class="button-link">Register</router-link>
-        </p>
-      </div>
-    </div>
+  <v-app id="app">
+    <v-container class="login-container" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" md="6" lg="4">
+          <v-card class="login-box" elevation="10">
+            <v-card-title class="headline title">Login</v-card-title>
+            <v-card-text>
+              <v-form @submit.prevent="login">
+                <v-text-field
+                  label="Email"
+                  v-model="form.email"
+                  required
+                  type="email"
+                  outlined
+                  class="input-field"
+                ></v-text-field>
+                <v-text-field
+                  label="Password"
+                  v-model="form.password"
+                  required
+                  type="password"
+                  outlined
+                  class="input-field"
+                ></v-text-field>
+                <v-btn
+                  type="submit"
+                  color="primary"
+                  block
+                  large
+                  class="login-btn"
+                  >Login</v-btn
+                >
+              </v-form>
+              <v-divider class="my-4"></v-divider>
+              <div class="switch-link">
+                Don't have an account?
+                <router-link to="/register">
+                  <v-btn text color="primary">Register</v-btn>
+                </router-link>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     <router-view />
-  </div>
+  </v-app>
 </template>
 
 <script
--- src/views/AuthPages/RegisterView.vue
+++ src/views/AuthPages/RegisterView.vue
@@ -1,53 +1,76 @@
 <template>
-  <div id="app">
-    <div class="register-container">
-      <div class="register-box">
-        <h2>Register</h2>
-        <form @submit.prevent="register">
-          <div class="input-group">
-            <label for="firstname">First Name</label>
-            <input
-              type="text"
-              id="firstname"
-              v-model="form.firstname"
-              required
-            />
-          </div>
-          <div class="input-group">
-            <label for="lastname">Last Name</label>
-            <input type="text" id="lastname" v-model="form.lastname" required />
-          </div>
-          <div class="input-group">
-            <label for="job">Job</label>
-            <input type="text" id="job" v-model="form.job" required />
-          </div>
-          <div class="input-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" v-model="form.email" required />
-          </div>
-          <div class="input-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              v-model="form.password"
-              required
-            />
-          </div>
-          <button type="submit">Register</button>
-        </form>
-        <p class="switch-link">
-          Already have an account?
-          <router-link to="/login" class="button-link">Login</router-link>
-        </p>
-      </div>
-    </div>
+  <v-app id="app">
+    <v-container class="register-container" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" md="6" lg="4">
+          <v-card class="register-box" elevation="10">
+            <v-card-title class="headline title">Register</v-card-title>
+            <v-card-text>
+              <v-form @submit.prevent="register">
+                <v-text-field
+                  label="First Name"
+                  v-model="form.firstname"
+                  required
+                  outlined
+                  class="input-field"
+                ></v-text-field>
+                <v-text-field
+                  label="Last Name"
+                  v-model="form.lastname"
+                  required
+                  outlined
+                  class="input-field"
+                ></v-text-field>
+                <v-text-field
+                  label="Job"
+                  v-model="form.job"
+                  required
+                  outlined
+                  class="input-field"
+                ></v-text-field>
+                <v-text-field
+                  label="Email"
+                  v-model="form.email"
+                  required
+                  outlined
+                  type="email"
+                  class="input-field"
+                ></v-text-field>
+                <v-text-field
+                  label="Password"
+                  v-model="form.password"
+                  required
+                  outlined
+                  type="password"
+                  class="input-field"
+                ></v-text-field>
+                <v-btn
+                  type="submit"
+                  color="primary"
+                  block
+                  large
+                  class="register-btn"
+                  >Register</v-btn
+                >
+              </v-form>
+              <v-divider class="my-4"></v-divider>
+              <div class="switch-link">
+                Already have an account?
+                <router-link to="/login">
+                  <v-btn text color="primary">Login</v-btn>
+                </router-link>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     <router-view />
-  </div>
+  </v-app>
 </template>
+
 <script
   lang="ts"
   src="@/controllers/AuthControllers/RegisterController.ts"
 ></script>
-
 <style src="@/styles/RegisterStyle.css"></style>
