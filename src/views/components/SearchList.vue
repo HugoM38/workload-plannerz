ffaaccb09895902a9eddce448b8658b27f9c3190
Hugo <>
2024-06-19 16:39:03 +0200

    feat: improve member list in team edit

--- src/views/components/SearchList.vue
+++ src/views/components/SearchList.vue
@@ -6,41 +6,75 @@
       outlined
       class="bg-accent text-on-accent mb-4"
     ></v-text-field>
-    <h2>Membres de l'équipe</h2>
-    <v-list class="member-list">
-      <v-list-item
-        v-for="(member, index) in filteredTeamMembers"
-        :key="index"
-        class="member-list-item"
-      >
-        <v-list-item-content>
-          <v-list-item-title
-            >{{ member.firstname }} {{ member.lastname }}</v-list-item-title
+
+    <v-card class="member-card">
+      <v-card-title>Membres de l'équipe</v-card-title>
+      <v-card-text class="member-card-content">
+        <v-list class="member-list">
+          <v-list-item
+            v-for="(member, index) in filteredTeamMembers"
+            :key="index"
+            class="member-list-item"
           >
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-btn color="error" @click="removeMember(member)">Retirer</v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
-    <h2>Non-membres de l'équipe</h2>
-    <v-list class="member-list">
-      <v-list-item
-        v-for="(nonMember, index) in filteredNonMembers"
-        :key="index"
-        class="member-list-item"
-      >
-        <v-list-item-content>
-          <v-list-item-title
-            >{{ nonMember.firstname }}
-            {{ nonMember.lastname }}</v-list-item-title
+            <v-row align="center" justify="space-between" no-gutters>
+              <v-col>
+                <v-list-item-content>
+                  <v-list-item-title
+                    >{{ member.firstname }}
+                    {{ member.lastname }}</v-list-item-title
+                  >
+                  <v-list-item-subtitle>{{
+                    member.email
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-col>
+              <v-col cols="auto">
+                <v-list-item-action>
+                  <v-btn color="error" @click="removeMember(member)"
+                    >Retirer</v-btn
+                  >
+                </v-list-item-action>
+              </v-col>
+            </v-row>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+
+    <v-card class="member-card mt-4">
+      <v-card-title>Non-membres de l'équipe</v-card-title>
+      <v-card-text class="member-card-content">
+        <v-list class="member-list">
+          <v-list-item
+            v-for="(nonMember, index) in filteredNonMembers"
+            :key="index"
+            class="member-list-item"
           >
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-btn color="primary" @click="addMember(nonMember)">Ajouter</v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+            <v-row align="center" justify="space-between" no-gutters>
+              <v-col>
+                <v-list-item-content>
+                  <v-list-item-title
+                    >{{ nonMember.firstname }}
+                    {{ nonMember.lastname }}</v-list-item-title
+                  >
+                  <v-list-item-subtitle>{{
+                    nonMember.email
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-col>
+              <v-col cols="auto">
+                <v-list-item-action>
+                  <v-btn color="primary" @click="addMember(nonMember)"
+                    >Ajouter</v-btn
+                  >
+                </v-list-item-action>
+              </v-col>
+            </v-row>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+
     <v-snackbar v-model="snackbar" :timeout="6000" top>
       {{ error }}
       <v-btn color="red" @click="snackbar = false">Fermer</v-btn>
@@ -54,6 +88,18 @@
 ></script>
 
 <style scoped>
+.member-card {
+  background-color: #c6c5c5;
+  border: 4px solid #dd7373;
+  max-height: 400px;
+  overflow-y: auto;
+}
+
+.member-card-content {
+  max-height: 300px;
+  overflow-y: auto;
+}
+
 .member-list {
   background-color: inherit;
   padding: 8px;
